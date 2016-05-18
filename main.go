95055ed8a723a01e483247192d33a452ff723f61
Akshatha Sreeramaprasad <xakv@nordstrom.net>
2016-05-18 15:14:51 -0700

    things
    Signed-off-by: Toby Rumans <adq3@nordstrom.net>

--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("HELLO")
+	http.HandleFunc("/", handler)
+
+	log.Fatal(http.ListenAndServe(":12345", nil))
+}
+
+func outerHandler(w http.ResponseWriter, req *http.Request) {
+	w.Write([]byte("hello"))
 }
